53600ef5433b9670a1f5a0c0d9ebb3e51f74a759
oliver <oli932300102@gmail.com>
2023-07-31 23:47:59 -0500

    add sidebar

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Dashboard.css';
+
+const DashBoardComponent = () =>{
+    return (
+        <div className='dashboard'>
+            <h1 className="text-3xl text-center font-bold underline">
+                DASHBOARD COMPONENT!
+            </h1>
+        </div>
+      );
+};
+
+
+export default DashBoardComponent;
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css';
+
+const FooterComponent = () => {
+    return(
+        <div>
+            <iframe class="power_bi" title="gesi_proyeto" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZTNmYTAxODUtYzc4ZC00YjAxLThlYWQtNzBlMzA0N2VkNDliIiwidCI6ImM1ZDIzYjA5LWFmMDEtNGFlYy1hYjc0LTdhNWQxZGEwMTA4NCJ9" frameborder="0" allowFullScreen="true"></iframe>
+        </div>
+    )
+};
+
+export default FooterComponent;
