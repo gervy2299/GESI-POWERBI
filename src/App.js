cdeb7c1e98329dd29af635dc6a78f5b1ff6a5feb
Gervy Salinas <play_main_1001@outlook.com>
2023-08-02 15:41:51 -0500

    correct component dashboard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 
 import DashBoardComponent from './components/dashboard/Dashboard.jsx';
-import FooterComponent from './components/footer/Footer.jsx';
 
 function App() {
   return (
     <div>      
       <DashBoardComponent/>
-      <FooterComponent/>
     </div>
   );
 }
