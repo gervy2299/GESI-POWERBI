19ee77d102896e4591b9071731c6ec56a48de0e7
Gervy Salinas <play_main_1001@outlook.com>
2023-08-07 13:40:51 -0500

    conexion con backend

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const FooterComponent = () => {
+function FooterComponent(){
     return(
-        <div>
-            <iframe class="power_bi" title="gesi_proyeto" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZTNmYTAxODUtYzc4ZC00YjAxLThlYWQtNzBlMzA0N2VkNDliIiwidCI6ImM1ZDIzYjA5LWFmMDEtNGFlYy1hYjc0LTdhNWQxZGEwMTA4NCJ9" frameborder="0" allowFullScreen="true"></iframe>
+        <div class="outline outline-offset-2 outline-1">
+            <iframe class="power_bi" title="gesi_proyeto" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZTNmYTAxODUtYzc4ZC00YjAxLThlYWQtNzBlMzA0N2VkNDliIiwidCI6ImM1ZDIzYjA5LWFmMDEtNGFlYy1hYjc0LTdhNWQxZGEwMTA4NCJ9"></iframe>
         </div>
     )
 };
